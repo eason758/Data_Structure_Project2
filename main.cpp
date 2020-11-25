ed66878d4ec971ad18fbc5a1fb078cf040d9f2a4
eason758 <61646301+eason758@users.noreply.github.com>
2020-11-25 22:20:58 +0800

    11/25 fin
    final ver

--- main.cpp
+++ main.cpp
@@ -6,7 +6,6 @@
 #include "func.h"
 #include "robot.h"
 
-#include <time.h>
 
 
 using namespace std;
@@ -18,51 +17,26 @@ void init(Floor& floor);
 void run(Floor& floor);
 void output(Floor& floor);
 
-
-
-double OUTPUT_START;
-double OUTPUT_END;
-
-
-
-
-
-
-void debug(Floor& floor);
-
 int main(int argc, char *argv[]) {
-
-	START = clock();
 
 	f.open(argv[1]);
 	Floor floor;
 	
-
 	init(floor);
 
 	run(floor);
 
-	double PROCESS_END = clock();
-	
 	output(floor);
 
-	END = clock();
-	
-	//debug(floor);
-	cout << "程式跑的時間:\n" << (PROCESS_END - PROCESS_START) / CLOCKS_PER_SEC << endl;
-	cout << "總共跑的時間:\n" << (END - START)/CLOCKS_PER_SEC << endl;
-	cout << "輸出共跑的時間:\n" << (OUTPUT_END - OUTPUT_START) / CLOCKS_PER_SEC << endl;
 	return 0;
 }
 
 
 void init(Floor& floor){
-	cout << "Initializing..." << endl;
+
 	floor.unclean_size = 0;
 	int r, c, b;
 	f >> r >> c >> b;
-
-
 
 	floor.map = new cell**[r];
 	for (int i = 0; i < r; i++) floor.map[i] = new cell*[c];
@@ -82,121 +56,18 @@ void init(Floor& floor){
 	}
 
 	floor.init(r, c, b);
-	cout << "Floor init success!" << endl;
-
 
 }
 
 void run(Floor& floor) {
 
-	//while (floor.unclean_size() > 0) {
-	//
-		floor.clear();				//clean一次 = 出去又回來
-	//
-	//}
-
+		floor.clear();
 }
 
 void output(Floor& floor) {
-	Node* cur = floor.get_root();
-	
-	vector<coor> path;
 
-	int pre_x(cur->cel->loc.x - 1), pre_y(cur->cel->loc.y);
-	int step = -1;
-	while (cur != nullptr)
-	{
-		if ( abs(cur->cel->loc.x - pre_x) > 1) {
-			cout << "wrong!" << endl;
-			unsigned int i = 0;
-			path.push_back(cur->cel->loc);
-			while (i < path.size())
-			{
-				cout << path[i];
-				i++;
-			}
-			OUTPUT_END = clock();
-			exit(1);
-		}
-		if (abs(cur->cel->loc.y - pre_y) > 1) {
-			cout << "wrong!" << endl;
-			unsigned int i = 0;
-			path.push_back(cur->cel->loc);
-			while (i < path.size())
-			{
-				cout << path[i];
-				i++;
-			}
-			OUTPUT_END = clock();
-			exit(1);
-		}
-		if (abs(cur->cel->loc.x - pre_x) + abs(cur->cel->loc.y - pre_y) > 1) {
-			cout << "wrong!" << endl;
-			unsigned int i = 0;
-			path.push_back(cur->cel->loc);
-			while (i < path.size())
-			{
-				cout << path[i];
-				i++;
-			}
-			OUTPUT_END = clock();
-			exit(1);
-		}
-		if (abs(cur->cel->loc.x - pre_x) + abs(cur->cel->loc.y - pre_y) == 0) {
-			cout << "wrong! same step!" << endl;
-			unsigned int i = 0;
-			path.push_back(cur->cel->loc);
-			while (i < path.size())
-			{
-				cout << path[i];
-				i++;
-			}
-			OUTPUT_END = clock();
-			exit(1);
-		}
-		if (floor.map[cur->cel->loc.x][cur->cel->loc.y]->attr == WALL) {
-			cout << "wrong! WALL!" << endl;
-			unsigned int i = 0;
-			path.push_back(cur->cel->loc);
-			while (i < path.size())
-			{
-				cout << path[i];
-				i++;
-			}
-			OUTPUT_END = clock();
-			exit(1);
-		}
-		
-		pre_x = cur->cel->loc.x;
-		pre_y = cur->cel->loc.y;
-		path.push_back(cur->cel->loc);
-		cur = cur->child;
-		step++;
-	}
+	cout << floor.path.size()-1 << endl;
 
-
-	cout << step << endl;
-	o << step << endl;
-	cur = floor.get_root();
-	OUTPUT_START = clock();
-	while (cur != nullptr)
-	{
-	//	cout << cur->cel->loc.y << " " << cur->cel->loc.x << endl;
-		o << cur->cel->loc.y << " " << cur->cel->loc.x << endl;
-		cur = cur->child;
-	}
-	OUTPUT_END  = clock();
-
-	cout << "list 輸出共跑的時間:\n" << (OUTPUT_END - OUTPUT_START) / CLOCKS_PER_SEC << endl;
-	
-	OUTPUT_START = clock();
-	unsigned int i = 0;
-	while (i < path.size())
-	{
-		o << path[i];
-		i++;
-	}
-	OUTPUT_END = clock();
-
-	cout << "vec 輸出共跑的時間:\n" << (OUTPUT_END - OUTPUT_START) / CLOCKS_PER_SEC << endl;
+	for(unsigned int i = 0 ; i < floor.path.size() ; i++)
+		o << floor.path[i].y<< " "<< floor.path[i].x << endl;
 }
